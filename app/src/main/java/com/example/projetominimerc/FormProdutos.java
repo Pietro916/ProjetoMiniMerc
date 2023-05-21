8da327a66752cfb11d74b04607fb38540349c046
Pietro916 <pietrorantin916@gmail.com>
2023-05-21 18:30:45 -0300

    Produtos pt1

---
+++ app/src/main/java/com/example/projetominimerc/FormProdutos.java
@@ -0,0 +1,14 @@
+package com.example.projetominimerc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FormProdutos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_produtos);
+    }
+}
