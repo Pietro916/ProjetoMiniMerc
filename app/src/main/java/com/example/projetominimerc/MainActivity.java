f5df3edab92ae94efe528e6bf780a4181a07cfa9
Pietro916 <pietrorantin916@gmail.com>
2023-05-23 15:57:27 -0300

    Conexões (Mercado1 com Produtos)

--- app/src/main/java/com/example/projetominimerc/MainActivity.java
+++ app/src/main/java/com/example/projetominimerc/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.projetominimerc;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AppCompatButton btn_mercado1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_mercado1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this,FormProdutos.class);
+                startActivity(intent);
+            }
+        });
     }
 }
