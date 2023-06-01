0a9e1fc3f75dedce6943fa7dec8d982e5fca1740
Nicolas-2701 <ngalvao388@gmail.com>
2023-06-01 17:13:36 -0300

    save

--- app/src/main/java/com/fieb/mercado/DatabaseManager.java
+++ app/src/main/java/com/fieb/mercado/DatabaseManager.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class DatabaseManager {
 
@@ -11,23 +13,27 @@ public class DatabaseManager {
     private static final String DB_USERNAME = "Nicolas_2701_SQLLogin_1";
     private static final String DB_PASSWORD = "m2rv8z283k";
 
-    public static void insertData(String descricao,Double preco,int quantidade,String nome, String image) {
-        try {
-            Connection conn = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+    private static final Executor executor = Executors.newSingleThreadExecutor();
 
-            String query = "INSERT INTO produto (descricao, nome, preco, quantidade, image) VALUES (?, ?, ?, ?, ?)";
-            PreparedStatement statement = conn.prepareStatement(query);
-            statement.setString(1, descricao);
-            statement.setString(2, nome);
-            statement.setDouble(3, preco);
-            statement.setInt(4, quantidade);
-            statement.setString(5, image);
-            statement.executeUpdate();
+    public static void insertData(String descricao, Double preco, int quantidade, String nome, String image) {
+        executor.execute(() -> {
+            try {
+                Connection conn = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
 
-            statement.close();
-            conn.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+                String query = "INSERT INTO usuarios (descricao, nome, preco, quantidade, image) VALUES (?, ?, ?, ?, ?)";
+                PreparedStatement statement = conn.prepareStatement(query);
+                statement.setString(1, descricao);
+                statement.setString(2, nome);
+                statement.setDouble(3, preco);
+                statement.setInt(4, quantidade);
+                statement.setString(5, image);
+                statement.executeUpdate();
+
+                statement.close();
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        });
     }
 }
